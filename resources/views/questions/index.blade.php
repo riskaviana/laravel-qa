02e55c595bfbabfe56e97676ada0c86112101552
Riska_Octaviana <riskaoctaviana7@gmail.com>
2019-03-30 00:28:01 +0700

    Build Question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,8 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+                    <div class="d-flex align-item-center">
+                        <h2>All Questions</h2>
+                        <div class="mx-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="card-body">
                     @foreach ($questions as $question)
                     <div class="media">
