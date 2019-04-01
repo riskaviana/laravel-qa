5c4bcf229879bbbe9d2302954176a7b68f8dcc3c
Riska_Octaviana <riskaoctaviana7@gmail.com>
2019-04-01 14:50:04 +0700

    display answers for question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,21 +3,45 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-15">
             <div class="card">
                 <div class="card-header">
-                    <div class="d-flex align-item-center">
+                    <div class="d-flex align-items-center">
                         <h1>{{ $question->title }}</h1>
-                        <div class="ml-auto">
+                            <div class="ml-auto">
                             <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back To All Question</a>
                         </div>
+                        </div>
+                    </div>
+                <div class="card-body">
+                        {!! $question->body !!}
+                        <hr>
+                    <div class="card-title">
+                        <h2>{{ $question->answers_count . " " . str_plural('Answer', $question->answers_count) }}</h2>
+                </div>
+                
+                @foreach ($question->answers as $answer)
+                    <div class="media">
+                        <div class="media-body">
+                            {!! $answer->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}"class="pr-2">
+                                        <img src="{{ $question->user->avatar }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}">{{ $answer->user->name }}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
-                <div class="card-body">
-                        {!! $question->body_html !!}
-                        </form>                   
-                        </div>
-                </div>
+                <hr>
+                @endforeach
             </div>
         </div>
     </div>
