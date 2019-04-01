86d4e35ce29c68ce616dfb4549e8ddfa7105f627
Riska_Octaviana <riskaoctaviana7@gmail.com>
2019-04-01 10:47:39 +0700

    Generate fake answers

--- app/User.php
+++ app/User.php
@@ -34,4 +34,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Answer::class);
     }
+    
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->slug);
+    }
 }
