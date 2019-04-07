f26c3ce5b79ca4d3f1e900455604820584508462
Riska_Octaviana <riskaoctaviana7@gmail.com>
2019-04-07 13:04:22 +0700

    Implement delete function

--- app/Answer.php
+++ app/Answer.php
@@ -26,6 +26,15 @@ class Answer extends Model
     	static::created(function ($answer) {
     		$answer->question->increment('answers_count');
     	});
+
+    	static::deleted(function ($answer) {
+    		$answer->question->decrement('answers_count');
+    	});
+    }
+
+    public function getCreatedDateAttribute()
+    {
+    	return $this->created_at->diffForHumans();
     }
 }
 
