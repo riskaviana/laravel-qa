95559d741f0b3dedefd55dc9720c867d545c2d36
Riska_Octaviana <riskaoctaviana7@gmail.com>
2019-04-29 11:03:28 +0700

    Securing the application from xss attack

--- app/Question.php
+++ app/Question.php
@@ -20,6 +20,11 @@ class Question extends Model
     	$this->attributes['slug'] = str_slug($value);
     }
 
+    //public function setBodyAttribute($value)
+    //{
+    	//$this->attribute['body'] = clen($value);
+    //}
+
     public function getUrlAttribute()
     {
     	return route("questions.show", $this->slug);
@@ -41,7 +46,7 @@ class Question extends Model
     }
     public function getBodyHtmlAttribute()
     {
-    	return \Parsedown::instance()->text($this->body);
+    	return clean($this->bodyHtml());
     }
     public function answers()
     {
@@ -75,5 +80,18 @@ class Question extends Model
     	return $this->favorites->count();
     }
 
-    
+    public function getExcerptAttribute()
+    {
+    	return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {
+    	return str_limit(strip_tags($this->bodyHtml()), $length);
+    }
+
+    private function bodyhtml()
+    {
+    	return \Parsedown::instance()->text($this->body);
+    }
 }
